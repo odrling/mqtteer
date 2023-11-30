496584897d484a73f889390eeb16ebe928677fa9
odrling <florianbadie@odrling.xyz>
2023-11-30 21:51:10 +0100

    add sensors to reports

--- mqtteer.c
+++ mqtteer.c
@@ -4,6 +4,7 @@
 #include <libproc2/misc.h>
 #include <locale.h>
 #include <mosquitto.h>
+#include <sensors/sensors.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +13,7 @@
 
 #define MOSQ_KEEPALIVE 30
 #define DISCOVERY_TOPIC_PREFIX "homeassistant"
+#define SENSORS_BUF_SIZE 200
 
 static char *mqtteer_device_name;
 static struct mosquitto *mosq;
@@ -63,7 +65,7 @@ void mqtteer_get_unique_id(char *unique_id, char *name, char *device_name) {
   sprintf(unique_id, "%s_%s", device_name, name);
 }
 
-void mqtteer_send_discovery(char *name, char *device_class, char *unit_of_measurement) {
+void mqtteer_send_discovery(char *name, const char *device_class, const char *unit_of_measurement) {
   char unique_id[mqtteer_unique_id_len(name, mqtteer_device_name)];
   mqtteer_get_unique_id(unique_id, name, mqtteer_device_name);
   char state_topic[mqtteer_state_topic_len()];
@@ -78,7 +80,7 @@ void mqtteer_send_discovery(char *name, char *device_class, char *unit_of_measur
   cJSON_AddStringToObject(discovery_obj, "state_topic", state_topic);
   cJSON_AddStringToObject(discovery_obj, "unique_id", unique_id);
 
-  #define TEMPLATE_STR_FORMAT "{{ value_json.%s }}"
+  #define TEMPLATE_STR_FORMAT "{{ value_json['%s'] }}"
   char template_str[strlen(name) + strlen(TEMPLATE_STR_FORMAT) - 1];
   sprintf(template_str, TEMPLATE_STR_FORMAT, name);
   cJSON_AddStringToObject(discovery_obj, "value_template", template_str);
@@ -98,11 +100,98 @@ void mqtteer_send_discovery(char *name, char *device_class, char *unit_of_measur
   cJSON_AddItemToObject(discovery_obj, "device", device_obj);
 
   char *discovery_payload = cJSON_Print(discovery_obj);
+  printf("%s\n", discovery_payload);
   mqtteer_send(discovery_topic, discovery_payload);
 
   free(discovery_payload);
   cJSON_Delete(discovery_obj);
 }
+
+struct mqtteer_sensor {
+  char* name;
+  double value;
+  const char* device_class;
+  const char* unit;
+};
+
+// Device classes
+static const char* TEMPERATURE = "temperature";
+
+// UNITS
+static const char* CELSIUS = "°C";
+
+void mqtteer_sensor_free(struct mqtteer_sensor* sensor) {
+  free(sensor->name);
+  free(sensor);
+}
+
+void mqtteer_sensors_init() {
+  int ret;
+
+  ret = sensors_init(NULL);
+  if (ret != 0) {
+    perror("sensors_init failed");
+    exit(EXIT_FAILURE);
+  }
+}
+
+char *mqtteer_sensor_get_name(char* chip_name, char* label) {
+  int len = strlen(chip_name) + strlen(label) + 2;
+  char* name = malloc(len);
+
+  snprintf(name, len, "%s_%s", chip_name, label);
+  return name;
+}
+
+struct mqtteer_sensor* mqtteer_get_sensor(int* nr_chip, int* nr_feat) {
+  const sensors_chip_name* chip;
+  const sensors_feature *feature;
+  const sensors_subfeature *sf;
+  struct mqtteer_sensor *sensor;
+  char name_buf[SENSORS_BUF_SIZE];
+  char* label;
+  char* sensor_name;
+
+  while ((chip = sensors_get_detected_chips(NULL, nr_chip)) != NULL) {
+    sensors_snprintf_chip_name(name_buf, SENSORS_BUF_SIZE, chip);
+
+    while ((feature = sensors_get_features(chip, nr_feat)) != NULL) {
+
+      label = sensors_get_label(chip, feature);
+      sensor_name = mqtteer_sensor_get_name(name_buf, label);
+      free(label);
+
+      switch (feature->type) {
+        case SENSORS_FEATURE_TEMP:
+          sf = sensors_get_subfeature(chip, feature, SENSORS_SUBFEATURE_TEMP_INPUT);
+          if (sf) {
+            sensor = malloc(sizeof(struct mqtteer_sensor));
+
+            sensor->name = sensor_name;
+            sensor->device_class = TEMPERATURE;
+            sensor->unit = CELSIUS;
+            sensors_get_value(chip, sf->number, &sensor->value);
+            printf("found %s\n", sensor_name);
+
+            return sensor;
+          } else {
+            printf("%s: could not get subfeature\n", sensor_name);
+          }
+          break;
+        default:
+          printf("%s: unsupported feature type: %d\n", sensor_name,
+                 feature->type);
+      }
+
+      free(sensor_name);
+    }
+
+    *nr_feat = 0;
+  }
+
+  return NULL;
+}
+
 
 char * mqtteer_getenv(char *name) {
   char *value = getenv(name);
@@ -114,14 +203,23 @@ char * mqtteer_getenv(char *name) {
 }
 
 void mqtteer_announce_topics() {
-    printf("announcing this device\n");
+  int nr_chip = 0, nr_feat = 0;
+  struct mqtteer_sensor* sensor;
+  printf("announcing this device\n");
 
-    mqtteer_send_discovery("uptime", "duration", "s");
-    mqtteer_send_discovery("load1", "power_factor", NULL);
-    mqtteer_send_discovery("load5", "power_factor", NULL);
-    mqtteer_send_discovery("load15", "power_factor", NULL);
-    mqtteer_send_discovery("used_memory", "data_size", "kB");
-    mqtteer_send_discovery("total_memory", "data_size", "kB");
+  mqtteer_send_discovery("uptime", "duration", "s");
+  mqtteer_send_discovery("load1", "power_factor", NULL);
+  mqtteer_send_discovery("load5", "power_factor", NULL);
+  mqtteer_send_discovery("load15", "power_factor", NULL);
+  mqtteer_send_discovery("used_memory", "data_size", "kB");
+  mqtteer_send_discovery("total_memory", "data_size", "kB");
+
+  mqtteer_sensors_init();
+  while ((sensor = mqtteer_get_sensor(&nr_chip, &nr_feat)) != NULL) {
+    mqtteer_send_discovery(sensor->name, sensor->device_class, sensor->unit);
+    mqtteer_sensor_free(sensor);
+  }
+  sensors_cleanup();
 }
 
 void mqtteer_report_metrics() {
@@ -130,6 +228,8 @@ void mqtteer_report_metrics() {
   double av1, av5, av15;
   unsigned long used, total;
   char state_topic[mqtteer_state_topic_len()];
+  int nr_chip = 0, nr_feat = 0;
+  struct mqtteer_sensor* sensor;
 
   procps_loadavg(&av1, &av5, &av15);
   procps_uptime(&uptime, NULL);
@@ -149,6 +249,13 @@ void mqtteer_report_metrics() {
   cJSON_AddNumberToObject(state_obj, "load15", av15);
   cJSON_AddNumberToObject(state_obj, "used_memory", used);
   cJSON_AddNumberToObject(state_obj, "total_memory", total);
+
+  mqtteer_sensors_init();
+  while ((sensor = mqtteer_get_sensor(&nr_chip, &nr_feat)) != NULL) {
+    cJSON_AddNumberToObject(state_obj, sensor->name, sensor->value);
+    mqtteer_sensor_free(sensor);
+  }
+  sensors_cleanup();
 
   char *payload = cJSON_Print(state_obj);
 
