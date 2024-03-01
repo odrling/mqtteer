0e5fc8fc7d55f4172a04c03ac3ee6c62bbfc23a7
odrling <florianbadie@odrling.xyz>
2024-03-01 09:05:10 +0100

    psi: make sure we can't read more than we can

--- mqtteer.c
+++ mqtteer.c
@@ -377,7 +377,7 @@ int mqtteer_psi_get(const char kind[], struct mqtteer_psi *psi) {
     return psi_fd;
   }
 
-  ssize_t count = read(psi_fd, buf, PSI_BUF_SIZE);
+  ssize_t count = read(psi_fd, buf, PSI_BUF_SIZE - 1);
   buf[count] = '\0';
   if (count <= 0) {
     perror("failed to read PSI");
